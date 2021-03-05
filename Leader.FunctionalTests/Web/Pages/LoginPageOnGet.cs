30679a27fc83b523bb853dcfd6a41122c13d9668
Peter Jones <peter.jones@loyaltylogistix.com>
2021-03-05 14:29:57 +0000

    Setup initial functional test

---
+++ Leader.FunctionalTests/Web/Pages/LoginPageOnGet.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Smeat.Leader.FunctionalTests.Web.Pages
+{
+    [Collection("Sequential")]
+    public class LoginPageOnGet : IClassFixture<WebTestFixture>
+    {
+        public LoginPageOnGet(WebTestFixture factory)
+        {
+            Client = factory.CreateClient();
+        }
+
+        public HttpClient Client { get; }
+
+        [Fact]
+        public async Task ReturnsLoginPage()
+        {
+            // Arrange & Act
+            var response = await Client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Contains("Log in", stringResponse);
+        }
+    }
+}
